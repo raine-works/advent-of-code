3c4a54af9286af10cb5704c38482a1099fc60ff0
Raine <raineforest01@gmail.com>
2020-12-08 16:53:32 -0700

    Started day 3

--- 2020/day-03/index.js
+++ 2020/day-03/index.js
@@ -1,2 +1,40 @@
 const getData = require('../../auth/index');
 const url = 'https://adventofcode.com/2020/day/3/input';
+
+const array = []; 
+
+const run = async () => {
+  const input = await getData(url);
+
+  let str = '';
+  for (i = 0; i < input.length; i++) {
+    let index = input.charAt(i);
+    if (index.match(/\n/) && str !== '') {
+      array.push(str.toString());
+      str = '';
+    } else {
+      str += input.charAt(i);
+    };
+  };
+  
+  let result = await checkPath(3, 1);
+  console.log(result);
+};
+
+function checkPath(right, down) {
+  let trees = 0;
+  let x = 0;
+  let y = 0;
+  let row = array[y];
+  let point = row.charAt(x);
+  while (y <= array.length) {
+    x += right;
+    y += down;
+    if (point == '#') {
+      trees++;
+    };
+  };
+  return trees;
+};
+
+module.exports = run;
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const dayOne = require('./2020/day-01/index');
 const dayTwo = require('./2020/day-02/index');
+const dayThree = require('./2020/day-03/index');
 
 // Run day one challenge...
-dayOne();
+// dayOne();
 
 // Run day two challenge... 
-dayTwo();
+// dayTwo();
+
+// Run day three challenge... 
+dayThree();
